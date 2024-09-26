56d7855c8485790412abadb976884e849f82a5cb
Torato Kitahara <tkitahar@c3r3s4.42tokyo.jp>
2024-09-26 13:13:59 +0900

    fixed init.c norminette

--- src/init.c
+++ src/init.c
@@ -6,53 +6,22 @@
 /*   By: tkitahar <tkitahar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 16:17:13 by tkitahar          #+#    #+#             */
-/*   Updated: 2024/09/24 19:08:02 by tkitahar         ###   ########.fr       */
+/*   Updated: 2024/09/26 13:10:25 by tkitahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fractol.h"
-
-static void	malloc_error(void)
-{
-	perror("Problem whith malloc");
-	exit(EXIT_FAILURE);
-}
 
 static void	init_data(t_fractol *fractol)
 {
 	fractol->escape_value = 4;
 }
 
-static void	my_mlx_init(t_fractol *fractol){}
-
 void	init_fractol(t_fractol *fractol)
 {
-	fractol->mlx_conection = mlx_init();
-	if (!fractol->mlx_conection)
-		malloc_error();
-	fractol->mlx_window = mlx_new_window(fractol->mlx_conection,
-			WIDTH,
-			HIGHT,
-			fractol->title);
-	if (!fractol->mlx_window)
-	{
-		mlx_destroy_display(fractol->mlx_conection);
-		free(fractol->mlx_conection);
-		malloc_error();
-	}
-	fractol->data.data_ptr = mlx_new_image(fractol->mlx_conection,
-			WIDTH,
-			HIGHT);
-	if (!fractol->data.data_ptr)
-	{
-		mlx_destroy_window(fractol->mlx_conection, fractol->mlx_window);
-		mlx_destroy_display(fractol->mlx_conection);
-		free(fractol->mlx_conection);
-		malloc_error();
-	}
-	fractol->data.image_ptr = mlx_get_data_addr(fractol->data.data_ptr,
-			&fractol->data.bits_per_pixel,
-			&fractol->data.bits_per_line,
-			&fractol->data.endian);
+	my_mlx_init(fractol);
+	my_mlx_new_window(fractol);
+	my_mlx_new_image(fractol);
+	my_mlx_get_data_addr(fractol);
 	init_data(fractol);
 }
---
+++ src/my_mlx.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkitahar <tkitahar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 13:01:10 by tkitahar          #+#    #+#             */
+/*   Updated: 2024/09/26 13:05:57 by tkitahar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/fractol.h"
+
+static void	malloc_error(void)
+{
+	perror("Problem whith malloc");
+	exit(EXIT_FAILURE);
+}
+
+void	my_mlx_init(t_fractol *fractol)
+{
+	fractol->mlx_conection = mlx_init();
+	if (!fractol->mlx_conection)
+		malloc_error();
+}
+
+void	my_mlx_new_window(t_fractol *fractol)
+{
+	fractol->mlx_window = mlx_new_window(fractol->mlx_conection,
+			WIDTH,
+			HIGHT,
+			fractol->title);
+	if (!fractol->mlx_window)
+	{
+		mlx_destroy_display(fractol->mlx_conection);
+		free(fractol->mlx_conection);
+		malloc_error();
+	}
+}
+
+void	my_mlx_new_image(t_fractol *fractol)
+{
+	fractol->data.data_ptr = mlx_new_image(fractol->mlx_conection,
+			WIDTH,
+			HIGHT);
+	if (!fractol->data.data_ptr)
+	{
+		mlx_destroy_window(fractol->mlx_conection, fractol->mlx_window);
+		mlx_destroy_display(fractol->mlx_conection);
+		free(fractol->mlx_conection);
+		malloc_error();
+	}
+}
+
+void	my_mlx_get_data_addr(t_fractol *fractol)
+{
+	fractol->data.image_ptr = mlx_get_data_addr(fractol->data.data_ptr,
+			&fractol->data.bits_per_pixel,
+			&fractol->data.bits_per_line,
+			&fractol->data.endian);
+}
